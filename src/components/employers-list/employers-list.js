16ed88a8de809e2a981a3a19ad1fd2e15ba588b7
Joramba <jorkabozko@gmail.com>
2021-09-30 14:33:13 +0200

    add dynamic EmployeesListItem and some things

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,12 +1,18 @@
 import EmployeesListItem from '../employers-list-item/employers-list-item';
 import './employers-list.css';
 
-const EmployersList = () => {
+const EmployersList = ({ data }) => {
+
+    const elements = data.map(item => {
+        return (
+            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase} />
+            //{...item}
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
