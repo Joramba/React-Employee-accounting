eb9d21890ab05c8936db3245d7e8df940c0f86d5
Joramba <jorkabozko@gmail.com>
2021-10-02 11:06:57 +0200

    add functionality to buttons

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -2,11 +2,10 @@ import EmployeesListItem from '../employers-list-item/employers-list-item';
 import './employers-list.css';
 
 const EmployersList = ({ data }) => {
-
     const elements = data.map(item => {
+        const { id, ...itemProps } = item;
         return (
-            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase} />
-            //{...item}
+            <EmployeesListItem key={id} {...itemProps} />
         )
     });
 
